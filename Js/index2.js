64a3aeb878ec44aa7681cb7b45633010c063079b
Scyies <scyies3@gmail.com>
2022-04-10 13:13:04 -0300

    todos os produtos ta mostrando os produtos agora

---
+++ Js/index2.js
@@ -0,0 +1,45 @@
+let listaProdutos = [];
+const searchBar = document.querySelector('.header-searchBar');
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredProducts = listaProdutos.filter((produto) => {
+        return (
+            produto.nome.toLowerCase().includes(searchString)
+        );
+    });
+    mostrarProdutos(filteredProducts);
+    console.log(filteredProducts);
+});
+// não sei como funcionaria na home, se for o caso teria que jogar para a página de todos os produtos e lá sim listar.
+
+
+const carregaProdutos = async () => {
+    try {
+        const res = await fetch('http://localhost:3000/produtos');
+        listaProdutos = await res.json();
+        mostrarProdutos(listaProdutos);
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
+const mostrarProdutos = (produtos) => {
+    produtos.forEach((produto) => {
+        const row = document.querySelector('.produtos');
+        const div = document.createElement("div");
+        div.classList.add("produto");
+
+        div.innerHTML = `
+                <img class='imagem-produto' src='${produto.imagem}'></img>
+                <p class='produto-nome'>${produto.nome}</p>
+                <p class='produto-preco'>${produto.preco}</p>
+                <a href='descricao-produtos-page.html' class='produto-verProduto--link'><p class='produto-verProduto'>${produto.descricao}</p>
+                `;
+        row.appendChild(div);
+
+    })}
+
+carregaProdutos();

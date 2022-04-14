971a5a936b2eaa0eac0a36929ef0c32a95f25883
Scyies <scyies3@gmail.com>
2022-04-13 21:14:13 -0300

    latualização do botão na home

--- Js/controllerProdutos-home.js
+++ Js/controllerProdutos-home.js
@@ -1,15 +1,16 @@
 import { produtosService } from "./produtosService.js";
 
-const criaNovoProduto = (imagem, nome, preco, descricao) => {
+const criaNovoProduto = (imagem, nome, preco, descricao, id) => {
     const linhaProdutoNovo = document.createElement('div');
     linhaProdutoNovo.classList.add("produto");
     const conteudo = `
     <img class='imagem-produto' src='${imagem}'></img>
     <p class='produto-nome'>${nome}</p>
     <p class='produto-preco'>${preco}</p>
-    <a href='descricao-produto.html' class='produto-verProduto--link'><p class='produto-verProduto'>${descricao}</p>
+    <button id='verProdutobtn' onclick='mostrarProdutoDescricao()' class='produto-verProduto--link produto-verProduto'>${descricao}</button>
     `;
     linhaProdutoNovo.innerHTML = conteudo;
+    linhaProdutoNovo.dataset.id = id;
     return linhaProdutoNovo;
 }
 

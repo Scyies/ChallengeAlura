5a7cd077bd818350753206e5d8b37aefe0647d83
Scyies <scyies3@gmail.com>
2022-04-23 19:33:09 -0300

    Revert "s"
    This reverts commit 85ca34a3756d3973c3b152db77c1a5309f51a5e9.

--- Js/fireBased.js
+++ Js/fireBased.js
@@ -1,22 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYxucnxHmgCX4fPPDL1JFhCnXqVcSG4Cg",
-  authDomain: "alurageek-test.firebaseapp.com",
-  databaseURL: "https://alurageek-test-default-rtdb.firebaseio.com",
-  projectId: "alurageek-test",
-  storageBucket: "alurageek-test.appspot.com",
-  messagingSenderId: "941261808244",
-  appId: "1:941261808244:web:03e5598d3b729d8e986721",
-  measurementId: "G-X5D53K656V"
+    
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
